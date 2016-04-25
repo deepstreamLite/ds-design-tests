323100cb6c459ee49746747a55e1ea0d6594cf53
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-04-25 07:55:57 +0200

    bigger header

--- js/start.js
+++ js/start.js
@@ -1,30 +1,46 @@
 $(function() {
-	new CodeSampleAnimation();
+	function applySizes() {
+		var windowHeight = $(window).height();
+		var navHeight = $( 'nav' ).height();
+		var conceptSelectHeight = $( '#concept-select' ).height();
+		// $( 'header' ).height( windowHeight - ( navHeight + conceptSelectHeight ) );
+				// $( 'header' ).height( windowHeight - ( navHeight + conceptSelectHeight ) );
+		 $( 'header' ).height( windowHeight - 241 );
+	}
 
-	return;
+	applySizes();
+	$(window).load(applySizes);
+	$(window).load(function(){
+		$('#loading-cover').fadeOut( 600 );
+	});
+	$(window).resize( applySizes );
+	new CodeSampleAnimation();
+return;
+
 	new DsLogo( 'nav .logo .container', {
-		radius: 16,
-		ballRadius: 1.8,
+		radius: 18,
+		ballRadius: 1.7,
 		angleOffset: 0.3,
-		cX: 17,
-		cY: 18,
+		cX: 25,
+		cY: 25,
 		attrD: {
 		//	stroke: 'rgba(255,255,255,0.8)',
 		//	fill: 'rgba(255,255,255,0.4)',
-			stroke: "#ddd",
-			fill: "#ddd",
+			stroke: "#46D3FA",
+			fill: "#46D3FA",
 			fillRule: 'evenodd',
-			strokeWidth: 0.8
+			strokeWidth: 1.2
 		},
 		attrS: {
-			stroke: 'gradient:l(0, 0, 1, 1)#8CE3FC-#0FBBEC',
+			stroke: '#0FBBEC',
 		//	fill: 'rgba(255,255,255,0.3)',
-			fill: 'gradient:l(0, 0, 1, 1)#8CE3FC-#0FBBEC',
+			fill: '#0FBBEC',
 			fillRule: 'evenodd',
-			strokeWidth: 0.8,
+			strokeWidth: 1.2,
 			'stroke-alignment': 'inner'
 		}
 	});
+
 
 	var codeSampleLogo = new DsLogo( '#code-samples .ds-logo', {
 		radius: 96,
