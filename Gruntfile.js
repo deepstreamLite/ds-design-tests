5e785cdd38d5b7dc46f8fc2f68cf129be8dca6dd
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-04-27 06:42:53 +0200

    switching to less

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			tasks: [ 'build' ],
+			files: [ './index.html', './less/**' ],
+			options: { livereload: 5051 },
+		},
+		clean: {
+			css: {
+				src: './css/*'
+			}
+		},
+		less: {
+			dev: {
+				files: {
+					'./css/screen.css': './less/main.less'
+				}
+			}
+		},
+		browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        './css/*.css'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: 'local.dev'
+                }
+            }
+        }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+	grunt.registerTask( 'build', [ 'less:dev' ] );
+	grunt.registerTask( 'default', ['browserSync', 'watch' ] );
+
+};
